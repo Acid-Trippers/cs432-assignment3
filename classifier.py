2d1bfb91177f05a0e58337cdd2059c4399507847
Satyakam-Mishra <mishra.satyakam03@gmail.com>
2026-03-17 09:35:57 +0530

    Added comments in classifier.py and ingesion.py

---
+++ classifier.py
@@ -0,0 +1,4 @@
+"""
+- Routes the data by looking at the metadata and sends it to SQL engine or Mongo Engine
+- For data which is not yet decided, the data will be sent to Buffer
+"""
---
+++ ingestion.py
@@ -0,0 +1,8 @@
+"""
+- Looks at initial_schema.json based on which it uses one of its data generations and chooses one of them for that particular field. 
+- Sends the data as json after choosing all the field's datatypes.
+- Uses the fieldname defined by the user as in initial_schema.json
+
+Input: initial_schema.json
+Output: None
+"""
